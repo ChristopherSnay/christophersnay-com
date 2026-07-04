99e193dbf17917ce3f8ef28f7735b20e481eead2
Christopher Snay <christopher.snay@gmail.com>
2026-07-04 12:25:59 -0400

    Removes app header.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import { Paper } from "@mui/material";
-import { Outlet, useLocation } from "react-router-dom";
-import AppFooter from "./components/AppFooter";
-import AppHeader from "./components/AppHeader";
+import { Paper } from '@mui/material';
+import { Outlet, useLocation } from 'react-router-dom';
+import AppFooter from './components/AppFooter';
 
 function App() {
   const location = useLocation();
   const mainClassName =
-    location.pathname == "/"
-      ? "d-flex flex-column flex-grow-1 w-100"
-      : "container d-flex flex-column flex-grow-1 w-100 mt-3";
+    location.pathname == '/'
+      ? 'd-flex flex-column flex-grow-1 w-100'
+      : 'container d-flex flex-column flex-grow-1 w-100 mt-3';
 
   return (
     <>
-      <AppHeader />
+      {/* <AppHeader /> */}
       <Paper component="main" className={mainClassName} elevation={0}>
         <Outlet />
       </Paper>
